7e9a79140a82ba9a6f8aa6763aabf541026e54ea
Clare Sudbery <clareworking@claresudbery.co.uk>
2022-02-14 11:00:06 +0000

    Refactor: Replace two tests with one test using test cases

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -12,27 +12,21 @@ describe Bowling do
         expect(result).to eq(0)
     end
 
-    it "after one roll, score is equal to number of pins" do
-        # Arrange
-        bowling = described_class.new
-        expected_score = 3
+    expected_scores_for_rolls = {
+        "2" => 2,
+        "5" => 5
+    }
 
-        # Act
-        result = bowling.get_score("#{expected_score}")
-
-        # Assert
-        expect(result).to eq(expected_score)
-    end    
-
-    it "after one roll, score is equal to number of pins" do
-        # Arrange
-        bowling = described_class.new
-        expected_score = 6
-
-        # Act
-        result = bowling.get_score("#{expected_score}")
-
-        # Assert
-        expect(result).to eq(expected_score)
+    expected_scores_for_rolls.each do |rolls, expected_score|
+        it "after one roll, score is equal to number of pins" do
+            # Arrange
+            bowling = described_class.new
+    
+            # Act
+            result = bowling.get_score(rolls)
+    
+            # Assert
+            expect(result).to eq(expected_score)
+        end  
     end
 end
