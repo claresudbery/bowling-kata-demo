a6802546ca57b84eb269dec5f05e03fdf0402ef1
Clare Sudbery <clareworking@claresudbery.co.uk>
2022-02-14 10:54:06 +0000

    added new test case for different num of pins

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -22,5 +22,17 @@ describe Bowling do
 
         # Assert
         expect(result).to eq(expected_score)
+    end    
+
+    it "after one roll, score is equal to number of pins" do
+        # Arrange
+        bowling = described_class.new
+        expected_score = 6
+
+        # Act
+        result = bowling.get_score("#{expected_score}")
+
+        # Assert
+        expect(result).to eq(expected_score)
     end
 end
